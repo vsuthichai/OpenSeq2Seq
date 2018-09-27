b99e709eea534c70b234cc0a5989968f701fa090
Ubuntu <ubuntu@ip-10-0-0-168.us-west-2.compute.internal>
2018-09-27 00:12:12 +0000

    turn timeline on

--- example_configs/text2text/en-de/my-transformer-big-mixed.py
+++ example_configs/text2text/en-de/my-transformer-big-mixed.py
@@ -37,7 +37,7 @@ base_params = {
   #"dtype": tf.float32, # to enable mixed precision, comment this line and uncomment two below lines
   "dtype": "mixed",
   "loss_scaling": "Backoff",
-  "iter_size": 16,
+  "iter_size": 32,
 
   "optimizer": tf.contrib.opt.LazyAdamOptimizer,
   "optimizer_params": {
