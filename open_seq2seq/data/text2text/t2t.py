e74c82ad2a36441ccb30d6feb405eb80d9010a12
Ubuntu <ubuntu@ip-172-31-75-4.ec2.internal>
2018-10-25 09:08:55 +0000

    memory for each tfrecord

--- open_seq2seq/data/text2text/t2t.py
+++ open_seq2seq/data/text2text/t2t.py
@@ -58,7 +58,7 @@ import tensorflow as tf
 _FILE_SHUFFLE_BUFFER = 512
 # Buffer size for reading records from a TFRecord file. Each training file is
 # 7.2 MB, so 8 MB allows an entire file to be kept in memory.
-_READ_RECORD_BUFFER = 8 * 1000 * 1000
+_READ_RECORD_BUFFER = 2 * 1000 * 1000
 
 # Example grouping constants. Defines length boundaries for each group.
 # These values are the defaults used in Tensor2Tensor.
