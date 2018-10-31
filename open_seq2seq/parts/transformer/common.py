70f7ceae1cdb119025d1d7eaf4244fe1e31d336a
Victor Suthichai <suthiv@amazon.com>
2018-10-31 00:23:12 +0000

    fp16 layer norm

--- open_seq2seq/parts/transformer/common.py
+++ open_seq2seq/parts/transformer/common.py
@@ -18,21 +18,22 @@ class LayerNormalization(tf.layers.Layer):
 
   def build(self, _):
     self.scale = tf.get_variable("layer_norm_scale", [self.hidden_size],
-                                 initializer=tf.ones_initializer(dtype=tf.float32),
-                                 dtype=tf.float32)
+                                 initializer=tf.ones_initializer(dtype=tf.float16),
+                                 dtype=tf.float16)
     self.bias = tf.get_variable("layer_norm_bias", [self.hidden_size],
-                                initializer=tf.zeros_initializer(dtype=tf.float32),
-                                dtype=tf.float32)
+                                initializer=tf.zeros_initializer(dtype=tf.float16),
+                                dtype=tf.float16)
     self.built = True
 
   def call(self, x, epsilon=1e-6):
-    dtype = x.dtype
-    x = tf.cast(x=x, dtype=tf.float32)
+    #dtype = x.dtype
+    #x = tf.cast(x=x, dtype=tf.float32)
     mean = tf.reduce_mean(x, axis=[-1], keepdims=True)
     variance = tf.reduce_mean(tf.square(x - mean), axis=[-1], keepdims=True)
     norm_x = (x - mean) * tf.rsqrt(variance + epsilon)
     result = norm_x * self.scale + self.bias
-    return tf.cast(x=result, dtype=dtype)
+    return result
+    #return tf.cast(x=result, dtype=dtype)
 
 
 class PrePostProcessingWrapper(object):
