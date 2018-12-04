6b511e24e772173a235af26e7e0c3d032afdae8b
Victor Suthichai <suthiv@amazon.com>
2018-12-04 18:49:07 +0000

    fast bn

--- open_seq2seq/parts/transformer/common.py
+++ open_seq2seq/parts/transformer/common.py
@@ -6,8 +6,32 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from tensorflow.python.framework import common_shapes
+from tensorflow.python.framework import ops
+import os
+from .layer_norm_fused_layer import layer_norm_custom
+
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+#loading the custom op library
+#custom_module = tf.load_op_library(os.path.join(dir_path, 'layer_norm_fused_op.so'))
+
+#This line is needed so TensorFlow can infer the shape of the output.
+#This may not be required (may even raise error) if you are using newer version of TensorFlow.
+#tf.RegisterShape("LayerNormCustom")(common_shapes.call_cpp_shape_fn)
+
+#register gradients for auto-differentiation.
+#@ops.RegisterGradient("LayerNormCustom")
+#def _LayerNormCustomGrad(op, grad):
+    #return [custom_module.layer_norm_backprop_custom(
+            #op.inputs[0], grad, op.get_attr("epsilon"))]
+
+#input_shape = [32,512,128]
+#inputs = tf.random_normal(input_shape)
+#normalized_output = custom_module.layer_norm_custom(inputs, epsilon=1e-6)
 
 # Define defaults for parameters
+
+
 
 class LayerNormalization(tf.layers.Layer):
   """Applies layer normalization."""
@@ -26,45 +50,61 @@ class LayerNormalization(tf.layers.Layer):
     self.built = True
 
   def call(self, x, epsilon=1e-6):
-    '''
     dtype = x.dtype
     x = tf.cast(x=x, dtype=tf.float32)
+
+    '''
     mean = tf.reduce_mean(x, axis=[-1], keepdims=True)
     variance = tf.reduce_mean(tf.square(x - mean), axis=[-1], keepdims=True)
     norm_x = (x - mean) * tf.rsqrt(variance + epsilon)
     result = norm_x * self.scale + self.bias
     z = tf.cast(x=result, dtype=dtype)
-    #print(z)
-    return z
+    #return z
     '''
 
-    #x1 = tf.expand_dims(x, axis=2)
-    dtype = x.dtype
+    '''
+    x1 = tf.expand_dims(x, axis=2)
     y1 = tf.layers.batch_normalization(
-      #center=True,
-      #scale=True,
-      inputs=tf.cast(x, dtype=tf.float32),
-      #inputs=x1,
-      training=True,
+      inputs=x1,
+      #inputs=tf.cast(x1, dtype=tf.float32),
       axis=-1,
-      #momentum=0.95,
+      momentum=0.0,
       #epsilon=epsilon,
       #epsilon=0.001,
+      center=True,
+      scale=True,
+      training=False,
+      trainable=False,
     )
-    #y2 = tf.squeeze(y1, axis=[2])
-    y2 = tf.cast(y1, dtype=dtype)
-    #print(y2)
-    return y2
+    y2 = tf.squeeze(y1, axis=[2])
+    y2 = tf.cast(y2, dtype=dtype)
+    '''
+
+    a1 = layer_norm_custom(
+        inputs=x, 
+        center=True,
+        scale=True,
+        activation_fn=None,
+        trainable=True,
+        #epsilon=epsilon,
+        #begin_norm_axis=-1
+    )
+    a1 = tf.cast(a1, dtype=dtype)
+    return a1
 
     '''
-    return tf.contrib.layers.layer_norm(
+    #x = tf.cast(x=x, dtype=tf.float32)
+    result = tf.contrib.layers.layer_norm(
       inputs=x,
       center=True,
       scale=True,
       activation_fn=None,
-      trainable=True,
+      trainable=False,
       begin_norm_axis=-1
     )
+    z1 = tf.cast(x=result, dtype=dtype)
+    z1 = tf.Print(z1, [tf.shape(a1), a1, tf.shape(z1), z1, tf.shape(z), z])
+    return z1
     '''
 
 class PrePostProcessingWrapper(object):
